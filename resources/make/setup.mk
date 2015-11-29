55343e03149a2d3607541b1095b9a02fe18f23c5
Duncan McGreggor <oubiwann@gmail.com>
2015-11-29 15:27:38 -0600

    Updated deps setup.

--- resources/make/setup.mk
+++ resources/make/setup.mk
@@ -2,7 +2,6 @@ USER_LIBS=/root/.lfe/libs
 
 get-deps-prep:
 	mkdir -p $(DEPS_DIR)
-	ln -s $(LFE_HOME) $(DEPS_DIR)/lfe
 
 # Get all the dependencies with the appropriate versions; due to a
 # bug in rebar (see https://github.com/rebar/rebar/issues/170), it's
@@ -12,60 +11,88 @@ deps-lib:
 	cd $(DEPS_DIR) && git clone $(DEPS_REPO) $(DEPS_LIB)
 	cd $(DEPS_DIR)/$(DEPS_LIB) && git checkout tags/$(DEPS_TAG)
 
-deps-yaws:
-	DEPS_LIB=yaws \
-	DEPS_REPO=https://github.com/klacke/yaws.git \
-	DEPS_TAG=yaws-2.0.2 \
-	make deps-lib
-
-deps-ibrowse:
-	DEPS_LIB=ibrowse \
-	DEPS_REPO=https://github.com/cmullaparthi/ibrowse.git \
-	DEPS_TAG=v4.2.2 \
-	make deps-lib
-
-deps-color:
-	DEPS_LIB=color \
-	DEPS_REPO=https://github.com/julianduque/erlang-color.git \
-	DEPS_TAG=v0.2.0 \
-	make deps-lib
-
-deps-lager:
-	DEPS_LIB=lager \
-	DEPS_REPO=https://github.com/basho/lager.git \
-	DEPS_TAG=3.0.2 \
-	make deps-lib
-
-deps-goldrush:
-	DEPS_LIB=goldrush \
-	DEPS_REPO=https://github.com/DeadZen/goldrush.git \
-	DEPS_TAG=0.1.8 \
-	make deps-lib
-
-# deps-XXX:
-# 	DEPS_LIB=XXX \
-# 	DEPS_REPO=YYY \
-# 	DEPS_TAG=ZZZ \
+# deps-yaws:
+# 	DEPS_LIB=yaws \
+# 	DEPS_REPO=https://github.com/klacke/yaws.git \
+# 	DEPS_TAG=yaws-2.0.2 \
 # 	make deps-lib
 
-# deps-XXX:
-# 	DEPS_LIB=XXX \
-# 	DEPS_REPO=YYY \
-# 	DEPS_TAG=ZZZ \
+# deps-ibrowse:
+# 	DEPS_LIB=ibrowse \
+# 	DEPS_REPO=https://github.com/cmullaparthi/ibrowse.git \
+# 	DEPS_TAG=v4.2.2 \
 # 	make deps-lib
 
-# deps-XXX:
-# 	DEPS_LIB=XXX \
-# 	DEPS_REPO=YYY \
-# 	DEPS_TAG=ZZZ \
+# deps-color:
+# 	DEPS_LIB=color \
+# 	DEPS_REPO=https://github.com/julianduque/erlang-color.git \
+# 	DEPS_TAG=v0.2.0 \
 # 	make deps-lib
 
-get-deps-for-docker: get-deps-prep deps-yaws deps-ibrowse deps-lager \
-	goldrush
+# deps-lager:
+# 	DEPS_LIB=lager \
+# 	DEPS_REPO=https://github.com/basho/lager.git \
+# 	DEPS_TAG=3.0.2 \
+# 	make deps-lib
+
+# deps-goldrush:
+# 	DEPS_LIB=goldrush \
+# 	DEPS_REPO=https://github.com/DeadZen/goldrush.git \
+# 	DEPS_TAG=0.1.8 \
+# 	make deps-lib
+
+deps-exemplar:
+	DEPS_LIB=exemplar \
+	DEPS_REPO=https://github.com/lfex/exemplar.git \
+	DEPS_TAG=0.2.0 \
+	make deps-lib
+
+deps-lfest:
+	DEPS_LIB=lfest \
+	DEPS_REPO=https://github.com/lfex/lfest.git \
+	DEPS_TAG=0.2.1 \
+	make deps-lib
+
+deps-kla:
+	DEPS_LIB=kla \
+	DEPS_REPO=https://github.com/lfex/kla.git \
+	DEPS_TAG=0.4.2 \
+	make deps-lib
+
+deps-clj:
+	DEPS_LIB=clj \
+	DEPS_REPO=https://github.com/lfex/clj.git \
+	DEPS_TAG=0.2.1 \
+	make deps-lib
+
+deps-logjam:
+	DEPS_LIB=logjam \
+	DEPS_REPO=https://github.com/lfex/logjam.git \
+	DEPS_TAG=0.1.0 \
+	make deps-lib
+
+deps-lfe:
+	ln -s $(LFE_HOME) $(DEPS_DIR)/
+
+deps-lutil:
+	ln -s $(USER_LIBS)/lutil $(DEPS_DIR)/
+
+deps-ltest:
+	ln -s $(USER_LIBS)/ltest $(DEPS_DIR)/
+
+deps-lcfg:
+	ln -s $(USER_LIBS)/lcfg $(DEPS_DIR)/
+
+# get-deps-for-docker: get-deps-prep deps-yaws deps-ibrowse deps-lager \
+# 	deps-goldrush \
+
+get-deps-for-docker:
+	deps-lfe deps-lutil deps-lcfg deps-ltest deps-exemplar \
 
 compile-for-docker: get-deps-for-docker
+	rebar get-deps
 	rebar compile
-	cp $(USER_LIBS)/*/ebin/* $(APP_DIR)/ebin/
-	cp $(DEPS_DIR)/*/ebin/* $(APP_DIR)/ebin/
-	cp $(LFE_HOME)/ebin/* $(APP_DIR)/ebin/
+	#cp -v $(USER_LIBS)/*/ebin/* $(APP_DIR)/ebin/
+	cp -v $(DEPS_DIR)/*/ebin/* $(APP_DIR)/ebin/
+	#cp -v $(LFE_HOME)/ebin/* $(APP_DIR)/ebin/
 
